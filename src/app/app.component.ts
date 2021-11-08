d7e5305775b1be6a6ca7e9a73a8090d1b59846a9
unknown <sadickminnih@gmail.com>
2021-11-08 15:02:45 +0100

    cours angular

---
+++ src/app/app.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+
+
+/* interface Person{ name:string, age:number} */
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+
+
+export class AppComponent {
+  name:string = "khalid";
+  firstNumber: number = 4;
+
+   sum = (first:number, second:number)=>{
+     return first+second;
+   }
+  /*  khalid: Person = {name: "khalid", age:25}
+
+   persons: Person[] = [
+     this.khalid,
+    {name: "sadick", age:25},
+    {name: "tom", age:22},
+    {name: "motti", age:21},
+    {name: "hamit", age:15},
+   ]
+
+   constructor(){
+     this.khalid.age = 25;
+   }
+ */
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AppComponent } from './app.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    CommonModule,
+    BrowserModule,
+    BrowserAnimationsModule
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
